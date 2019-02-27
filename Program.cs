5109d8caa45bb0d485817c4f1304bc5438499c4e
Carlos MI <cmunoz@mother.travel>
2019-02-26 22:37:35 -0600

    Method is added - JS file is added

--- Program.cs
+++ Program.cs
@@ -2,11 +2,17 @@ using System;
 
 namespace TEST
 {
-	public class Program()
+	public class Program
 	{
 		public static void Main()
 		{
 			Console.WriteLine("KA");
+			SayName("Carlos");
+		}
+
+		public static string SayName(string name)
+		{
+			Console.WriteLine($"hello {name}");
 		}
 	}
 }
